e658dd2b756884af1f8a5fe3c8046d3155b1144a
Arnold  Patten <apatten001@yahoo.com>
2022-03-31 14:15:53 -0400

    update envirment Url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false, 
-  luv2shopApiUrl: "https://localhost:8443/api", 
+  production: false,
+  luv2shopApiUrl: 'https://horshambags-backend.azurewebsites.net/api'
   stripePublishableKey: "pk_test_51KgCFHJRGEhgeKCt4IGVHyOThBYvYMYCM22G7y7axfgfW2CeCpNsITGrzM624U3CJRFzQeKpr9uuXUc80lSy6nk300sPAYIY0i"
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
