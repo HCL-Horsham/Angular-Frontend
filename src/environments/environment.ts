b72363ef48422600be37780bd8f3a0f3fb521d46
xyil <44515243+xyil@users.noreply.github.com>
2022-04-04 09:47:19 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  luv2shopApiUrl: 'https://horshambags-backend.azurewebsites.net/api'
+  luv2shopApiUrl: 'https://horshambags-backend.azurewebsites.net/api',
   stripePublishableKey: "pk_test_51KgCFHJRGEhgeKCt4IGVHyOThBYvYMYCM22G7y7axfgfW2CeCpNsITGrzM624U3CJRFzQeKpr9uuXUc80lSy6nk300sPAYIY0i"
 };
 
