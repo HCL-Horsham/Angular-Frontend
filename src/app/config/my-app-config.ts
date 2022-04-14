9826f868eb36b53de752d1ce5ecb2ca23f339299
xyil <44515243+xyil@users.noreply.github.com>
2022-04-14 10:07:32 -0400

    Update my-app-config.ts

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -4,7 +4,7 @@ export default {
         clientId: '0oa46q823pISLnySn5d7',
         issuer: 'https://dev-4328024.okta.com/oauth2/default',
         redirectUri: "https://hclhorshambags-frontend.azurewebsites.net/login/callback",
-        scopes: ['openid', 'profile', 'email']
+        scopes: ['openid', 'profile', 'email', 'groups']
     }
 
 }
